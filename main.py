e37437d60d1fae5e90a30f942f2b8b39da81d20f
ravindra1976 <86829875+ravindra1976@users.noreply.github.com>
2026-03-31 17:34:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "6591994405:AAFSYBd9f-td2kIHxb583_oGqMEBSqv1rww"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello! I am running on JustRunMy.App 🚀")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
