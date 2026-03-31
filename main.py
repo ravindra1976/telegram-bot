4cab26b9ec99eb85b45dca49159880fc60d0b73e
ravindra1976 <86829875+ravindra1976@users.noreply.github.com>
2026-03-31 17:44:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "6591994405:AAFSYBd9f-td2kIHxb583_oGqMEBSqv1rww"
+import os
+TOKEN = os.getenv("TOKEN")
+
+# TOKEN = "6591994405:AAFSYBd9f-td2kIHxb583_oGqMEBSqv1rww"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! I am running on JustRunMy.App 🚀")
