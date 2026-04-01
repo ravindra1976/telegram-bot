9e0f11f50f549d928640042d40ff31c15a71c3c4
ravindra1976 <86829875+ravindra1976@users.noreply.github.com>
2026-04-01 10:42:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-import os
-TOKEN = os.getenv("TOKEN")
-
-# TOKEN = "6591994405:AAFSYBd9f-td2kIHxb583_oGqMEBSqv1rww"
+TOKEN = os.getenv("6591994405:AAFSYBd9f-td2kIHxb583_oGqMEBSqv1rww")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! I am running on JustRunMy.App 🚀")
+    await update.message.reply_text("Hello! I'm alive 🚀")
 
 app = ApplicationBuilder().token(TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
